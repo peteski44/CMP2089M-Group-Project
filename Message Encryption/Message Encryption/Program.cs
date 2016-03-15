8085a50a79d633c5c0d7732752b064fd7b4146aa
James Jassie <jpjassie@gmail.com>
2016-03-15 10:02:15 +0000

    Loading and Saving Text to text boxes
    Initial upload of Data Class and Program

---
+++ Message Encryption/Message Encryption/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Message_Encryption
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+            //My code starts here.
+            //Variables to store text from either text box.
+            string pTxtIn = messageBox.Text;
+            string eTxtIn = encryptedMessageBox.Text;
+
+            //Create instance of Data Class
+            Data textRead = new Data(pTxtIn, eTxtIn);
+        }
+    }
+}
