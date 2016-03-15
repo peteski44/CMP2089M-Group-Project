68a25b3c39408f0c1c6af6a7f96eff09dd5332ff
James Jassie <jpjassie@gmail.com>
2016-03-15 14:57:19 +0000

    Added methods to Click-Events
    Added methods for populating both text boxes and saving from the text
    boxes to file.

--- Message Encryption v1.1/Message Encryption/Form1.cs
+++ Message Encryption v1.1/Message Encryption/Form1.cs
@@ -25,18 +25,18 @@ namespace Message_Encryption
 
         private void messageBox_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void loadTextFile_Click(object sender, EventArgs e)
         {
-            //Set the parameters of the Open File Dialog
+            //Set parameters of the OpenFileDialog
             OpenFileDialog dataFile = new OpenFileDialog();
             dataFile.Filter = "Text Files (.txt)| *.txt";
             dataFile.Multiselect = false;
             dataFile.InitialDirectory = "c:\\";
 
-            //Show the Open File Dialog Box
+            //Show OpenFileDialog Box
             if (dataFile.ShowDialog() == DialogResult.OK)
                 messageBox.LoadFile(dataFile.FileName,
                     RichTextBoxStreamType.PlainText);
@@ -44,15 +44,15 @@ namespace Message_Encryption
 
         public void saveToText_Click(object sender, EventArgs e)
         {
-            //Set parameters for Save File Dialog
-            SaveFileDialog fileSave = new SaveFileDialog();
-            fileSave.DefaultExt = "*.txt";
-            fileSave.Filter = "Text Files|*.txt";
+            //Set parameters for SaveFileDialog
+            SaveFileDialog toTextFile = new SaveFileDialog();
+            toTextFile.DefaultExt = "*.txt";
+            toTextFile.Filter = "Text Files|*.txt";
 
-            //Show the Save File Dialog Box
-            if (fileSave.ShowDialog() == DialogResult.OK && fileSave.FileName.Length > 0)
+            //Show SaveFileDialog Box
+            if (toTextFile.ShowDialog() == DialogResult.OK && toTextFile.FileName.Length > 0)
             {
-                messageBox.SaveFile(fileSave.FileName, RichTextBoxStreamType.PlainText);
+                messageBox.SaveFile(toTextFile.FileName, RichTextBoxStreamType.PlainText);
             }
         }
 
@@ -63,12 +63,30 @@ namespace Message_Encryption
 
         private void loadTextFile2_Click(object sender, EventArgs e)
         {
+            //Set parameters of the OpenFileDialog
+            OpenFileDialog encDataFile = new OpenFileDialog();
+            encDataFile.Filter = "Text File (.txt)| *.txt";
+            encDataFile.Multiselect = false;
+            encDataFile.InitialDirectory = "c:\\";
 
+            //Show OpenFileDialog Box
+            if (encDataFile.ShowDialog() == DialogResult.OK)
+                encryptedMessageBox.LoadFile(encDataFile.FileName,
+                    RichTextBoxStreamType.PlainText);
         }
 
         private void saveToText2_Click(object sender, EventArgs e)
         {
+            //Set parameters for SaveFileDialog
+            SaveFileDialog encToTextFile = new SaveFileDialog();
+            encToTextFile.DefaultExt = "*.txt";
+            encToTextFile.Filter = "Text Files|*.txt";
 
+            //Show SaveFileDialog Box
+            if (encToTextFile.ShowDialog() == DialogResult.OK && encToTextFile.FileName.Length > 0)
+            {
+                encryptedMessageBox.SaveFile(encToTextFile.FileName, RichTextBoxStreamType.PlainText);
+            }
         }
 
         private void encryptText_Click(object sender, EventArgs e)
